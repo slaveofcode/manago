90e104777d20031d8ecabbc218cee1a5cab8e403
Aditya Kresna <slaveofcode@gmail.com>
2025-04-20 19:34:19 +0700

    initial config with astro, svelte & SSR mode using express

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+
+  adapter: node({
+    mode: 'middleware'
+  })
+});
---
+++ run-server.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+import { handler as ssrHandler } from './dist/server/entry.mjs';
+
+const app = express();
+// Change this based on your astro.config.mjs, `base` option.
+// They should match. The default value is "/".
+const base = '/';
+app.use(base, express.static('dist/client/'));
+app.use((req, res, next) => {
+    // pass locals to ssrHandler (Astro.locals)
+    const locals = {}; 
+    ssrHandler(req, res, next, locals);
+  });
+
+app.listen(8080);
